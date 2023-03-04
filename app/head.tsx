da4309ed6010903277e7ea8009ccc2de61d9f4c8
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-03-04 22:07:19 +0500

    link preview updated

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,13 @@ export default function Head() {
       <title>PIAIC</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Presidential Initiative for Artificial Intelligence and Computing (PIAIC)" />
+      <meta property="og:title" content="PIAIC" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://piaic-ultra.vercel.app/" />
+      <meta property="og:image" content="https://api.vercel.com/now/files/6ab3f3af82ef58545139437f92a21282fbc83546/ogimage.png" />
+      <meta property="og:image:alt" content="PIAIC" />
+      <meta property="og:image:width" content="402" />
+      <meta property="og:image:height" content="552" />
       <link rel="icon" href="/favicon.png" />
     </>
   )
